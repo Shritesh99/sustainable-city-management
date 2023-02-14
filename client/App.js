1b077322630fc0a238edb49a163729dabf4cc7e7
keumm <keumm@tcd.ie>
2023-02-14 19:41:40 +0000

    feat: test commit

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Navigation from './src/components/Navigation';
+//This is the Entry of the Application
+const App = () => {
+
+    return <Navigation />
+    
+
+}
+
+export default App;
