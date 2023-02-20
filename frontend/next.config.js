6e57dbf46a2f171e1d12dee4b0181bf64ea53cd2
Pookii <bingqi.xia@gmail.com>
2023-02-20 00:58:33 +0000

    fix: fix local request cors error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: `/api/:path*`, destination: `http://127.0.0.1:8080/:path*` },
+    ];
+  },
+
+  publicRuntimeConfig: {
+    apiUrl:
+		// process.env.NODE_ENV === "development"
+			// ? "" // development api
+			// : "", // production api
+      process.env.NODE_ENV === "development"
+        ? "http://127.0.0.1:8080" // development api
+        : "http://127.0.0.1:8080", // production api
+  },
 };
 
 module.exports = nextConfig;
