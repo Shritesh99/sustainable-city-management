641fb22417ad3fec231360d24879639a82a76f82
Prakhar Singh <prakhar.singh1928@gmail.com>
2023-03-21 18:48:10 +0000

    testcases

--- backend/internal/gateway-service/gateway_test.go
+++ backend/internal/gateway-service/gateway_test.go
@@ -3,10 +3,12 @@ package gateway_service
 import (
 	_ "github.com/Eytins/sustainable-city-management/backend/config"
 	_ "github.com/Eytins/sustainable-city-management/backend/pkg/logger"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestRegisterValidRequest(t *testing.T) {
@@ -90,4 +92,39 @@ func TestLoginRequestWrongPassword(t *testing.T) {
 	}
 	assert.Nil(t, err)
 	assert.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+}
+
+func TestGetProfileWithoutToken(t *testing.T) {
+	registerBody := `{
+        "username": "johasan2doe@example.com"
+    }`
+
+	// Iterate through test single test cases
+
+	resp, err := http.Post("http://127.0.0.1:8000/auth/profile", "application/json", strings.NewReader(registerBody))
+	if err != nil {
+		print(err)
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+}
+
+func GenerateToken() string {
+	expirationTime := time.Now().Add(5 * time.Hour)
+	// Create the JWT claims, which includes the username and expiry time
+	claims := &Claims{
+		Username: "abcde@gmail.com",
+		RegisteredClaims: jwt.RegisteredClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, _ := token.SignedString(jwtKey)
+	return tokenString
+	// Finally, we set the client cookie for "token" as the JWT we just generated
+
 }
