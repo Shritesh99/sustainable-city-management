e5254d7e759d059e4767ae3e4265f1a12e1680e5
Shritesh Jamulkar <shritesh.sj@gmail.com>
2023-04-11 21:43:20 +0100

    added healthcheck above

--- backend/internal/common-service/helthcheck.go
+++ backend/internal/common-service/helthcheck.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/Eytins/sustainable-city-management/backend/config"
 	"github.com/heptiolabs/healthcheck"
 )
 
@@ -38,26 +37,6 @@ func (a *service) configureHealthCheckEndpoints(ctx context.Context, health heal
 	for _, s := range a.cfg.ConnectedServices {
 		health.AddReadinessCheck(fmt.Sprintf("%s Health Readiness", s.ServiceName), healthcheck.HTTPGetCheck(fmt.Sprintf("http://%s:%s%s", s.ServiceUrl, s.HealthcheckPort, a.cfg.Probes.ReadinessPath), 5*time.Minute))
 		health.AddReadinessCheck(fmt.Sprintf("%s Health Liveness", s.ServiceName), healthcheck.HTTPGetCheck(fmt.Sprintf("http://%s:%s%s", s.ServiceUrl, s.HealthcheckPort, a.cfg.Probes.LivenessPath), 5*time.Minute))
-		quit := make(chan struct{})
-		ticker := time.NewTicker(5 * time.Second)
-		go func(s config.Services) {
-			for {
-				select {
-				case <-ticker.C:
-					if err := healthcheck.HTTPGetCheck(fmt.Sprintf("http://%s:%s%s", s.ServiceUrl, s.HealthcheckPort, a.cfg.Probes.ReadinessPath), 5*time.Minute)(); err != nil {
-						a.log.Errorf("(Health Liveness for %s) err: %v", s.ServiceName, err)
-					}
-					a.log.Infof("(Health Liveness for %s)", s.ServiceName)
-					if err := healthcheck.HTTPGetCheck(fmt.Sprintf("http://%s:%s%s", s.ServiceUrl, s.HealthcheckPort, a.cfg.Probes.LivenessPath), 5*time.Minute)(); err != nil {
-						a.log.Errorf("(Health Readiness for %s) err: %v", s.ServiceName, err)
-					}
-					a.log.Infof("(Health Readiness for %s)", s.ServiceName)
-				case <-quit:
-					ticker.Stop()
-					return
-				}
-			}
-		}(s)
 	}
 }
 
