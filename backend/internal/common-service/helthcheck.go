2671e195109c87a29e2fb7bbaa0e7332a9916d71
Shritesh Jamulkar <shritesh.sj@gmail.com>
2023-04-13 16:08:28 +0100

    Added healthcheck

--- backend/internal/common-service/helthcheck.go
+++ backend/internal/common-service/helthcheck.go
@@ -2,9 +2,7 @@ package common_service
 
 import (
 	"context"
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/heptiolabs/healthcheck"
 )
@@ -22,22 +20,12 @@ func (a *service) runHealthCheck(ctx context.Context) {
 		ReadTimeout:  readTimeout,
 	}
 
-	a.configureHealthCheckEndpoints(ctx, health)
-
 	go func() {
 		a.log.Infof("(%s) Kubernetes probes listening on port: %s", a.cfg.ServiceName, a.cfg.Probes.Port)
 		if err := a.healthCheckServer.ListenAndServe(); err != nil {
 			a.log.Errorf("(ListenAndServe) err: %v", err)
 		}
 	}()
-}
-
-func (a *service) configureHealthCheckEndpoints(ctx context.Context, health healthcheck.Handler) {
-	// TODO: Check db service
-	for _, s := range a.cfg.ConnectedServices {
-		health.AddReadinessCheck(fmt.Sprintf("%s Health Readiness", s.ServiceName), healthcheck.HTTPGetCheck(fmt.Sprintf("http://%s:%s%s", s.ServiceUrl, s.HealthcheckPort, a.cfg.Probes.ReadinessPath), 5*time.Minute))
-		health.AddReadinessCheck(fmt.Sprintf("%s Health Liveness", s.ServiceName), healthcheck.HTTPGetCheck(fmt.Sprintf("http://%s:%s%s", s.ServiceUrl, s.HealthcheckPort, a.cfg.Probes.LivenessPath), 5*time.Minute))
-	}
 }
 
 func (a *service) shutDownHealthCheckServer(ctx context.Context) error {
