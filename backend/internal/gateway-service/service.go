1c88d76b183058e5c2ff10cc0f6538c996f1c970
Eytins <eytins@foxmail.com>
2023-04-19 23:32:36 +0100

    Fix bug

--- backend/internal/gateway-service/service.go
+++ backend/internal/gateway-service/service.go
@@ -53,30 +53,30 @@ func NewGatewayService(router fiber.Router, store *db.SQLStore, cfg *config.Conf
 
 func CollectDataTimerTask(server *GatewayService, logger logger.Logger) {
 	for {
-		//err := server.SaveStationData()
-		//if err != nil {
-		//	logger.Fatal("Collect air station data failed")
-		//}
-		//time.Sleep(12 * time.Minute)
-		err := server.SaveNoiseData()
+		err := server.SaveStationData()
+		if err != nil {
+			logger.Fatal("Collect air station data failed")
+		}
+		time.Sleep(12 * time.Minute)
+		err = server.SaveNoiseData()
 		if err != nil {
 			logger.Fatal("Collect noise data failed")
 		}
-		//time.Sleep(12 * time.Minute)
-		//err = server.SaveBusData()
-		//if err != nil {
-		//	logger.Fatal("Collect bus data failed")
-		//}
-		//time.Sleep(12 * time.Minute)
-		//err = server.SaveBikeData()
-		//if err != nil {
-		//	logger.Fatal("Collect bike data failed")
-		//}
-		//time.Sleep(12 * time.Minute)
-		//err = server.ChangeBinStatus()
-		//if err != nil {
-		//	logger.Fatal("Change bin status failed")
-		//}
+		time.Sleep(12 * time.Minute)
+		err = server.SaveBusData()
+		if err != nil {
+			logger.Fatal("Collect bus data failed")
+		}
+		time.Sleep(12 * time.Minute)
+		err = server.SaveBikeData()
+		if err != nil {
+			logger.Fatal("Collect bike data failed")
+		}
+		time.Sleep(12 * time.Minute)
+		err = server.ChangeBinStatus()
+		if err != nil {
+			logger.Fatal("Change bin status failed")
+		}
 		time.Sleep(12 * time.Minute)
 	}
 }
